185b88f5a8ada8ba38d4124167f5d53e0b49cca1
Yurii_Yevchenko <yurievch@gmail.com>
2024-01-25 16:39:37 +0200

    создал хуки userstate, useEffect, usecontext. выводит список кроссовок из сервера. отображение и скрытие корзины при клике на кнопку Корзина.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import React from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
+
 function App() {
+  const [items, setItems] = React.useState([]);
+  const [cartItems, setCartItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://65b23fcb9bfb12f6eafd44bc.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
+  const onAddToCart = (obj) => {
+    setCartItems((prev) => [...prev, obj]);
+  };
+
   return (
     <div className="wrapper clear">
-      <Drawer />
-      <Header />
+      {cartOpened && (
+        <Drawer items={cartItems} onClose={() => setCartOpened(false)} />
+      )}
+      <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1 className="">Все кросовки</h1>
@@ -15,8 +37,16 @@ function App() {
           </div>
         </div>
 
-        <div className="d-flex">
-          <Card />
+        <div className="d-flex flex-wrap">
+          {items.map((item) => (
+            <Card
+              title={item.title}
+              price={item.price}
+              imageUrl={item.imageUrl}
+              onFavorite={() => console.log("Добавили в закладки")}
+              onPlus={(obj) => onAddToCart(obj)}
+            />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/components/Card/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./Card.module.scss";
+console.log(styles);
+function Card({ onFavorite, title, imageUrl, price, onPlus }) {
+  const [isAdded, setIsAdded] = React.useState(false);
+
+  const onClickPlus = () => {
+    onPlus({ title, imageUrl, price });
+    setIsAdded(!isAdded);
+  };
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.favorite} onClick={onFavorite}>
+        <img src="/img/heart-unliked.svg" alt="heart-unliked" />
+      </div>
+
+      <img width={133} height={112} src={imageUrl} alt="sneakers" />
+      <h5>{title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена</span>
+          <b>{price} грн</b>
+        </div>
+
+        <img
+          className={styles.plus}
+          onClick={onClickPlus}
+          src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
+          alt="plus"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
